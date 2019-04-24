c49ea6c9498b582b7da61a5390c292515a5b15ae
s1250137-Tasaki <s1250137@u-aizu.ac.jp>
2019-04-24 12:26:08 +0900

    shoukadai1_mikansei

--- ex1.c
+++ ex1.c
@@ -1,13 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-  int xp1,xp2,xq1,xq2,yp1,yp2,yq1,yq2,detA;
-  double x,y,s,t,EPS = 0.00000005;
+  int N,M,P,Q,x[4],y[4],i,j,b[2],e[2],detA;
+  double X,Y,s,t,EPS = 0.00000005;
 
-  //printf("input  : ");
-  scanf("%d %d %d %d %d %d %d %d",&xp1,&yp1,&xq1,&yq1,&xp2,&yp2,&xq2,&yq2);
+  scanf("%d %d %d %d",&N,&M,&P,&Q);
+  for(i = 0 ; i < N ; i++){
+    scanf("%d %d",&x[i],&y[i]);
+  }
 
-  detA = (xq1 - xp1)*(yp2 - yq2) + (xq2 - xp2)*(yq1 - yp1);
+  for(j = 0 ; j < M ; j++){
+    scanf("%d %d",&b[j],&e[j]);
+  }
+
+  for(i = 0 ; i < M ; i++){
+    for(j = 0 ; j < M ; j++){
+      if(i == b[j] - 1){
+	tmp = x[i];
+	X[i] == x[i];
+      }
+    }
+  }
+  
+  //detA = (x[1] - x[b[j] - 1])*(y[2] - y[3]) + (x[3] - x[2])*(y[1] - y[b[j] - 1]);
+
+  detA = (x[1] - x[0])*(y[2] - y[3]) + (x[3] - x[2])*(y[1] - y[0]);
+  //detA = (xq1 - xp1)*(yp2 - yq2) + (xq2 - xp2)*(yq1 - yp1);
   printf("detA = %d\n",detA);
 
   if(detA >= -EPS && detA <= EPS){
@@ -16,10 +34,12 @@ int main(){
     return 0;
   }
 
-  s = (double)((yp2 - yq2)*(xp2 - xp1) + (xq2 - xp2)*(yp2 - yp1))/(double)detA;
-  t = (double)((yp1 - yq1)*(xp2 - xp1) + (xq1 - xp1)*(yp2 - yp1))/(double)detA;
+  s = (double)((y[2] - y[3])*(x[2] - x[0]) + (x[3] - x[2])*(y[2] - y[0]))/(double)detA;
+  t = (double)((y[0] - y[1])*(x[2] - x[0]) + (x[1] - x[0])*(y[2] - y[0]))/(double)detA;
 
-  printf("s = %lf t = %lf\n",s,t);
+  /*s = (double)((yp2 - yq2)*(xp2 - xp1) + (xq2 - xp2)*(yp2 - yp1))/(double)detA;
+    t = (double)((yp1 - yq1)*(xp2 - xp1) + (xq1 - xp1)*(yp2 - yp1))/(double)detA;*/
+  //printf("s = %lf t = %lf\n",s,t);
 
   if((s <= 0 || s >= 1) || (t <= 0 || t >= 1)){
     //printf("交差無し2\n");
@@ -27,10 +47,12 @@ int main(){
     return 0;
   }
 
-  x = (double)xp1 + (double)(xq1 - yp1)*s;
-  y = (double)yp1 + (double)(yq1 - yp1)*t;
+  X = (double)x[0] + (double)(x[1] - y[0])*s;
+  Y = (double)y[0] + (double)(y[1] - y[0])*s;
 
-  printf("x = %lf y = %lf\n",x,y);
+  /*x = (double)xp1 + (double)(xq1 - yp1)*s;
+    y = (double)yp1 + (double)(yq1 - yp1)*s;*/
+  printf("x = %.5lf y = %.5lf\n",X,Y);
 
   return 0;
 

285f4d952afed6a2b2303b05d48e0d803ba3fb80
s1250137-Tasaki <s1250137@u-aizu.ac.jp>
2019-05-08 12:01:59 +0900

    shoukadai-1

--- ex1.c
+++ ex1.c
@@ -1,32 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-  int N,M,P,Q,x[4],y[4],i,j,b[2],e[2],detA;
+  int N,M,P,Q,x1[4],y1[4],x[4],y[4],i,j,b[2],e[2],detA;
   double X,Y,s,t,EPS = 0.00000005;
 
   scanf("%d %d %d %d",&N,&M,&P,&Q);
   for(i = 0 ; i < N ; i++){
-    scanf("%d %d",&x[i],&y[i]);
+    scanf("%d %d",&x1[i],&y1[i]);
   }
 
   for(j = 0 ; j < M ; j++){
     scanf("%d %d",&b[j],&e[j]);
   }
 
-  for(i = 0 ; i < M ; i++){
-    for(j = 0 ; j < M ; j++){
-      if(i == b[j] - 1){
-	tmp = x[i];
-	X[i] == x[i];
-      }
-    }
+  i = 0;
+  for(j = 0 ; j < M ; j++){
+    x[b[j] - 1] = x1[i];
+    y[b[j] - 1] = y1[i];
+    x[e[j] - 1] = x1[i + 1];
+    y[e[j] - 1] = y1[i + 1];
+    i += 2;
   }
-  
-  //detA = (x[1] - x[b[j] - 1])*(y[2] - y[3]) + (x[3] - x[2])*(y[1] - y[b[j] - 1]);
 
   detA = (x[1] - x[0])*(y[2] - y[3]) + (x[3] - x[2])*(y[1] - y[0]);
-  //detA = (xq1 - xp1)*(yp2 - yq2) + (xq2 - xp2)*(yq1 - yp1);
-  printf("detA = %d\n",detA);
+  //printf("detA = %d\n",detA);
 
   if(detA >= -EPS && detA <= EPS){
     //printf("交差無し1\n");
@@ -36,9 +33,6 @@ int main(){
 
   s = (double)((y[2] - y[3])*(x[2] - x[0]) + (x[3] - x[2])*(y[2] - y[0]))/(double)detA;
   t = (double)((y[0] - y[1])*(x[2] - x[0]) + (x[1] - x[0])*(y[2] - y[0]))/(double)detA;
-
-  /*s = (double)((yp2 - yq2)*(xp2 - xp1) + (xq2 - xp2)*(yp2 - yp1))/(double)detA;
-    t = (double)((yp1 - yq1)*(xp2 - xp1) + (xq1 - xp1)*(yp2 - yp1))/(double)detA;*/
   //printf("s = %lf t = %lf\n",s,t);
 
   if((s <= 0 || s >= 1) || (t <= 0 || t >= 1)){
@@ -50,8 +44,6 @@ int main(){
   X = (double)x[0] + (double)(x[1] - y[0])*s;
   Y = (double)y[0] + (double)(y[1] - y[0])*s;
 
-  /*x = (double)xp1 + (double)(xq1 - yp1)*s;
-    y = (double)yp1 + (double)(yq1 - yp1)*s;*/
   printf("x = %.5lf y = %.5lf\n",X,Y);
 
   return 0;

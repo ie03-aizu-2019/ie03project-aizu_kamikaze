8bde5d1244c859f36cd6409d2115d958fb6944d7
s1250137-Tasaki <s1250137@u-aizu.ac.jp>
2019-05-14 11:32:17 +0900

    shoukadai-2

--- ex1_2.c
+++ ex1_2.c
@@ -6,7 +6,7 @@ void calcDetA(int xp1,int yp1,int xq1,int yq1,int xp2, int yp2, int xq2, int yq2
 double s,t,detA;
 int main(){
   int N,M,P,Q,x[200],y[200],i,j,b[100],e[100],k = 0;
-  double X,Y,EPS = 0.00000005;
+  double X[1000],Y[1000],EPS = 0.00000005,tmp;
 
   scanf("%d %d %d %d",&N,&M,&P,&Q);
   for(i = 0 ; i < N ; i++){
@@ -23,17 +23,29 @@ int main(){
       calcDetA(x[b[i] - 1],y[b[i] - 1], x[e[i] - 1],y[e[i] - 1], x[b[j] - 1],y[b[j] - 1], x[e[j] - 1],y[e[j] - 1]);
 
       if(detA < -EPS || detA > EPS){
-	if((s >= 0.000000 && s <= 1.000000) && (t >= 0.000000 && t <= 1.000000)){
+	if((s > 0.000000 && s < 1.000000) && (t > 0.000000 && t < 1.000000)){
 	  
-	  X = (double)x[b[i] - 1] + (double)(x[e[i] - 1] - x[b[i] - 1])*s;
-	  Y = (double)y[b[i] - 1] + (double)(y[e[i] - 1] - y[b[i] - 1])*s;
-	  
-	  printf("x = %.5lf y = %.5lf\n",X,Y);
+	  X[k] = (double)x[b[i] - 1] + (double)(x[e[i] - 1] - x[b[i] - 1])*(double)s;
+	  Y[k] = (double)y[b[i] - 1] + (double)(y[e[i] - 1] - y[b[i] - 1])*(double)s;
+
+	  k++;
 	}
       }
-      
-      k++;
     }
+  }
+
+  for(i = 0 ; i < k ; i++){
+    for(j = i + 1 ; j < k ; j++){
+      if(X[i] > X[j]){
+	tmp = X[i];
+	X[i] = X[j];
+	X[j] = tmp;
+      }
+    }
+  }
+
+  for(i = 0 ; i < k ; i++){
+    printf("x = %.5lf y = %.5lf\n",X[i],Y[i]);
   }
 
   return 0;
@@ -41,12 +53,11 @@ int main(){
 }
 
 void calcDetA(int xp1,int yp1,int xq1,int yq1,int xp2, int yp2, int xq2, int yq2){
-  //double detA;
   detA = (xq1 - xp1)*(yp2 - yq2) + (xq2 - xp2)*(yq1 - yp1);
   //printf("detA = %lf\n",detA);
       
-  s = (double)((yp2 - yq2)*(xp2 - xp1) + (xq2 - xp2)*(yp2 - yp1))/detA;
-  t = (double)((yp1 - yq1)*(xp2 - xp1) + (xq1 - xp1)*(yp2 - yp1))/detA;
+  s = (double)((yp2 - yq2)*(xp2 - xp1) + (xq2 - xp2)*(yp2 - yp1))/(double)detA;
+  t = (double)((yp1 - yq1)*(xp2 - xp1) + (xq1 - xp1)*(yp2 - yp1))/(double)detA;
   //printf("s = %lf t = %lf\n",s,t);
 
 }
